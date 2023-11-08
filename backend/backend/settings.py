6bb385592a2638170f8548e3871dfc65787e5753
Mikhail Komissarov <datradexon@gmail.com>
2023-11-08 03:39:12 +0300

    Initial

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)
 
 DEBUG = os.getenv('DEBUG_VALUE') == 'True'
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,kommgramm.ru").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,taskikomiss.ddns.net").split(",")
 
 
 # Application definition
